6ec94905b17131c53f23859adcf2cf92f8122983
kryptocodes <srivatsantb@gmail.com>
2019-05-23 20:12:09 +0530

    nav bar works now

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
